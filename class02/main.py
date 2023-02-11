2bfa93ee94039153f54c85440a47ccbf2d6179fe
1andrewwh <123991669+1andrewwh@users.noreply.github.com>
2023-02-10 22:23:06 -0800

    Update main.py

--- class02/main.py
+++ class02/main.py
@@ -1,93 +1,127 @@
 import js
 p5 = js.window
 
+# Makes hair move in random positions every time the code runs
+rand = p5.random(80,90)
+rand_hair = p5.random(60,80)
+rand_hair2 = p5.random(40,60)
+rand_hair1 = p5.random(90,110)
+rand_hair3 = p5.random(140,160)
+rand_hair4 = p5.random(180,200)
+
 def setup():
     p5.createCanvas(300, 300)    # 300 x 300 pixel canvas 
 
+
 def draw():
-    white = 255
-    black = 0
-    light_grey = 170
-    medium_grey = 150
-    greyish_white = 210
-    dark_grey = 120
-    lighter_grey = 240
-    p5.background(103,12,21)
+    p5.background(230)
     p5.text(str(p5.mouseX) + ", " + str(p5.mouseY), 10, 20)
     p5.noStroke()
+    p5.push()
+    draw_ears()
+    draw_neck()
+    draw_face()
+    p5.pop()
+    draw_eyes()
+    draw_lips()
+    draw_nose()
+    side_hair()
+    p5.quad(77,113,101,90,rand,rand_hair2,rand_hair2,rand)
+    p5.quad(101,90,138,80,rand,rand_hair2,rand_hair2,rand)
+    p5.quad(138,80,179,84,rand_hair3,rand_hair2,rand,rand_hair2)
+    p5.quad(179,84,204,98,rand_hair4,rand_hair2,rand_hair1,rand_hair2)
+    p5.quad(204,98,227,118,243,86,rand_hair4,rand_hair2)
+    draw_shirt()
+    hand_cursor(p5.mouseX, p5.mouseY)
+
+def draw_face():
+    p5.translate(150,150)
+    for i in range(30):
+        p5.fill(234 - (i + 10),197 - (i+30),165 - (i + 40))
+        p5.rotate(p5.radians(360/30))
+        draw_skin()
+
+
+def draw_lips():
+    p5.push()
+    p5.translate(108, 186)
+    p5.fill(206,92,90)
+    p5.arc(40,0, 60,20, p5.radians(-10), p5.radians(190))
+    p5.pop()
+
+
+def hand_cursor(x,y):
+    p5.noStroke()
+    p5.fill(20)
+    p5.quad(x-2,y,x-2,y-40,x+2,y-40,x+2,y)
+    p5.fill(255)
+    p5.stroke(20)
+    p5.strokeWeight(2)
+    p5.fill(137,198,255,50)
+    p5.ellipse(x,y-80,80,80)
+
+
+def draw_eyes():
+    for i in range(1,4,2):
+        p5.fill(245)
+        p5.stroke(5)
+        p5.ellipse(213 - i*32, 124, 50, 20)
+        p5.fill(0)
+        p5.ellipse(213 - i*32, 124, 20, 20)
+        p5.fill(157,77,13)
+        p5.ellipse(213 - i *32, 124, 7, 7)
+
+
+def draw_shirt():
+    p5.fill(41,51,69)
+    p5.quad(18,300,32,255,150,300,30,300)
+    p5.line(32,254,103,229)
+    p5.fill(162, 175, 196)
+    p5.quad(32,254,103,229,150,300,30,300)
+    p5.fill(41,51,69)
+    p5.quad(123,300,280,300,262,254,200,234)
+
+
+def side_hair():
+    p5.fill(67,59,55)
+    p5.quad(62,86,80,104,80,157,64,146)
+    p5.quad(222,157,237,146,243,86,225,104)
+
+
+def draw_ears():
+    p5.fill(215,163,134)
+    p5.ellipse(64,136,20,80)
+    p5.ellipse(236,136,20,80)
+
+
+def draw_neck():
+    p5.fill(196,150,113)
+    p5.quad(108,220,194,220,216,296,78,296)
+
+
+def draw_skin():
+    p5.arc(20,10, 80, 140, p5.radians(40), p5.radians(190))
+
+def draw_nose():
+    p5.fill(234,197,165)
+    p5.quad(144,142,156,142,165,167,134,167)
     
-    # moon
-    p5.fill(251, 173, 16)
-    p5.ellipse(150, 150, 240 , 240)
-    
-    # body
-    p5.fill(light_grey)
-    p5.fill(greyish_white)
-    for i in range(3):
-        p5.triangle(105 + i * 34,129,140 + i *34,133, 112 + i * 34,142)
-    p5.fill(medium_grey+10)
-    for i in range(3):
-        p5.quad(121+i*34,181,132+i*34,179,146+i*34,205,117+i*34,192)
-    p5.fill(greyish_white)
-    p5.quad(214,205,224,180,241,200,236,213)
-    p5.fill(medium_grey-10)
-    p5.quad(180,205,185,191,208,208,197,216)
-    p5.fill(greyish_white)
-    for i in range(1,5):
-        p5.quad(44 + i * 34, 142, 73 + i * 34, 133, 98+ i * 34, 179, 88 + i * 34, 181)
-    p5.fill(light_grey+20)
-    for i in range(1,5):
-        p5.triangle(44 + i * 34, 142, 88 + i * 34, 180, 78+ i * 34, 205)
-    p5.fill(greyish_white-10)
-    
-    # legs
-    p5.fill(light_grey+10)
-    p5.quad(87, 180, 98, 178, 113, 207, 88, 196)
-    p5.fill(dark_grey)
-    p5.quad(73,163,87,180,80,194, 65,192)
-    p5.fill(dark_grey+10)
-    p5.quad(65,192,80,194,71,208,58,206)
-    p5.fill(205)
-    p5.quad(88,196,112,206,98,227,82,223)
-    
-    # head
-    p5.fill(light_grey)
-    p5.quad(45,142,74,136,91,138,60,147)
-    p5.triangle(60,147,78,142,78,163)
-    p5.quad(60,147,78,142,98,178,87,180)
-    p5.fill(lighter_grey-25)
-    p5.triangle(53, 120, 76, 125, 87, 112)
-    p5.triangle(87, 112, 53, 120, 76, 105)
-    p5.fill(lighter_grey-10)
-    p5.triangle(87, 112, 88, 93, 76, 105)
-    p5.triangle(87, 112, 95, 126, 69, 135)
-    p5.quad(58, 103, 76, 105, 53, 120, 41, 118)
-    p5.fill(greyish_white-5)
-    p5.quad(41, 118, 53, 120, 53, 131, 38, 133)
-    p5.fill(lighter_grey-10)
-    p5.quad(53, 120, 76, 125, 75, 136, 53, 131)
-    p5.quad(53, 120, 76, 125, 75, 136, 53, 131)
-    p5.quad(69, 93, 88, 93, 76, 105, 58, 103)
-    p5.quad(88, 94, 103, 97, 113, 111, 87, 112)
-    p5.fill(light_grey+10)
-    p5.triangle(44, 142, 52, 131, 73, 136)
-    p5.quad(87, 112, 113, 111, 116, 125, 95, 126)
-    p5.quad(76, 126, 116, 125, 113, 141, 74, 136)
-    p5.triangle(38,133,52,131,45,142)
-    
-    # eye
-    p5.fill(black)
-    p5.ellipse(85, 114, 8, 8)
-    p5.fill(white)
-    
-    # ears 
-    p5.fill(lighter_grey)
-    p5.quad(78, 50, 98, 51, 88, 93, 69, 93)
-    p5.fill(lighter_grey-20)
-    p5.quad(77, 51, 102, 12, 111, 8, 98, 51)
-    p5.fill(medium_grey)
-    p5.quad(88, 94, 98, 50, 123, 60, 102, 97)
-    p5.fill(dark_grey)
-    p5.quad(98, 50, 148, 14, 160, 18, 123, 60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
